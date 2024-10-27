d5503d5e0080835749a13e79043f70c8d28ecdef
EvstafevArtem <evstafev.as@phystech.edu>
2024-10-27 15:41:54 +0300

    did task

---
+++ src/main/java/Main.java
@@ -0,0 +1,30 @@
+import threadpool.FixedThreadPool;
+import threadpool.ScalableThreadPool;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static java.lang.Thread.sleep;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        int numTasks = 20;
+        final AtomicInteger count = new AtomicInteger(0);
+
+        FixedThreadPool threadpool = new ScalableThreadPool(2, 4);
+
+        threadpool.start();
+        for (int i = 0; i < numTasks; i++) {
+            threadpool.execute(() -> {
+                try {
+                    Thread.sleep(4_000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(count.getAndIncrement());
+            });
+        }
+        threadpool.stop();
+
+        System.out.println("result counter is " + count.get());
+    }
+}
